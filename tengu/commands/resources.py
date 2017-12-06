1bc0da67eea6362f55aead0b857438a622f463fb
sborny <sander.borny@ugent.be>
2017-12-06 14:57:27 +0100

    small fix

--- tengu/commands/resources.py
+++ tengu/commands/resources.py
@@ -13,7 +13,7 @@ class Resources(Base):
         url = 'http://' + self.get_host_ip() + ':5000/juju/applications'  # VERANDER NAAR /applications !!!!
         try:
             r = requests.get(url)
-            print(json.dumps(r.json(), indent=2, sort_keys=True))
+            print(dumps(r.json(), indent=2, sort_keys=True))
         except requests.exceptions.RequestException as e:
             print(e)
             exit(1)
